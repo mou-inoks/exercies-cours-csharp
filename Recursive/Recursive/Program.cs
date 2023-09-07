0a5e2ca38b37598099e17405024b33bbea8b8069
mou-inoks <mou@inokscapital.com>
2023-09-07 14:08:46 +0200

    finished recursive

---
+++ Recursive/Recursive/Program.cs
@@ -0,0 +1,26 @@
+﻿//Non recursive
+int fct1(int n)
+{
+    int multiplication = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        multiplication *= i;
+    }
+    return multiplication;
+}
+Console.WriteLine(fct1(5)); // 120
+
+
+// 1. Recursive mode 
+int fct2(int n)
+{
+    if (n == 1)
+        return 1;
+    else
+    {
+        return n * fct2(n - 1);
+    }
+}
+Console.WriteLine(fct2(5));
+
+
